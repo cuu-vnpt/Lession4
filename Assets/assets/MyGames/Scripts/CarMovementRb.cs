31bccee0f26a552220852b3cce26566005b7beef
cuu-vnpt <kunnobita97@gmail.com>
2024-04-09 23:41:20 +0700

    new lesssion

---
+++ Assets/assets/MyGames/Scripts/CarMovementRb.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarMovementRb : MonoBehaviour
+{
+    private Rigidbody rb;
+    public float speedPosition = 20;
+    public float speedRotation = 50;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float v = Input.GetAxis("Vertical");
+        float h = Input.GetAxis("Horizontal");
+        rb.MovePosition(rb.position + new Vector3(0, 0, v * Time.deltaTime * speedPosition));
+        rb.MoveRotation(transform.rotation * Quaternion.Euler(new Vector3(0, h * Time.deltaTime * speedRotation, 0)));
+    }
+}
